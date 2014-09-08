6e5522588795043fd0c5d3043683531cc20ec1bb
elp87 <elp1987@gmail.com>
2014-09-08 18:53:47 +0400

    Add Money

---
+++ elp87.Finance/elp87.Finance/Money.cs
@@ -0,0 +1,84 @@
+﻿namespace elp87.Finance
+{
+    public class Money
+    {
+        #region Fields
+        private decimal _value;
+        #endregion
+
+        #region Constructors
+        public Money(double value)
+        {
+            this._value = (decimal)value;
+        }
+
+        public Money(decimal value)
+        {
+            this._value = value;
+        }
+
+        public Money(int value)
+        {
+            this._value = value;
+        }
+        #endregion
+
+        #region Properties
+        public decimal Value { get { return _value; } set { _value = value; } }
+        #endregion
+
+        #region Operators
+        public static Money operator +(Money a, Money b)
+        {
+            return new Money(a.Value + b.Value);
+        }
+
+        public static Money operator -(Money a, Money b)
+        {
+            return new Money(a.Value - b.Value);
+        }
+
+        public static Money operator *(Money money, int mult)
+        {
+            return new Money(money.Value * mult);
+        }
+
+        public static Money operator *(Money money, double mult)
+        {
+            return new Money(money.Value * (decimal)mult);
+        }
+
+        public static Money operator /(Money money, int mult)
+        {
+            return new Money(money.Value / (decimal)mult);
+        }
+
+        public static Money operator /(Money money, double mult)
+        {
+            return new Money(money.Value / (decimal)mult);
+        }
+
+        public static double operator /(Money a, Money b)
+        {
+            return (double)(a.Value / b.Value);
+        }
+
+        #region Implicit
+        public static implicit operator Money(int value)
+        {
+            return new Money(value);
+        }
+
+        public static implicit operator Money(decimal value)
+        {
+            return new Money(value);
+        }
+
+        public static implicit operator Money(double value)
+        {
+            return new Money(value);
+        }
+        #endregion
+        #endregion
+    }
+}
