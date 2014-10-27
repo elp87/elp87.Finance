c0d99667b8e33f65ed989c0c587300be600d2a4e
elp87 <elp1987@gmail.com>
2014-10-27 17:44:04 +0400

    TradeDay: Add ctor(DateTime, Money, Money, TradeDay)

---
+++ elp87.Finance/Test.elp87.Finance/TradeDayTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using elp87.Finance;
+
+namespace Test.elp87.Finance
+{
+    [TestClass]
+    public class TradeDayTest
+    {
+        [TestMethod]
+        public void TestCtor()
+        {
+            List<TradeDay> tradeDays = new List<TradeDay>();
+            TradeDay day0 = new TradeDay(new DateTime(2014, 10, 20), 100000, 100000, null);
+            TradeDay day1 = new TradeDay(new DateTime(2014, 10, 21), 120000, 0, day0);
+            TradeDay day2 = new TradeDay(new DateTime(2014, 10, 22), 110000, 0, day1);
+            TradeDay day3 = new TradeDay(new DateTime(2014, 10, 23), 130000, 0, day2);
+            TradeDay day4 = new TradeDay(new DateTime(2014, 10, 24), 225000, 100000, day3);
+            tradeDays.Add(day0);
+            tradeDays.Add(day1);
+            tradeDays.Add(day2);
+            tradeDays.Add(day3);
+            tradeDays.Add(day4);
+
+            Money[] expCumProfit = new Money[] { 0, 20000, 10000, 30000, 25000 };
+            Money[] expDrawDown = new Money[] { 0, 0, 10000, 0, 5000 };
+
+            for (int i = 0; i < tradeDays.Count; i++)
+            {
+                Assert.AreEqual(expCumProfit[i], tradeDays[i].CumProfit);
+                Assert.AreEqual(expDrawDown[i], tradeDays[i].DrawDown);
+            }
+        }
+    }
+}
--- elp87.Finance/elp87.Finance/TradeDay.cs
+++ elp87.Finance/elp87.Finance/TradeDay.cs
@@ -10,6 +10,40 @@ namespace elp87.Finance
         private double _cumProfitPC;
         #endregion
 
+        #region Constructors
+        public TradeDay()
+        {
+        }
+
+        public TradeDay(DateTime date,
+            Money capital,
+            Money introduce,
+            TradeDay previousDay)
+        {
+            this._date = date;
+            this._capital = capital;
+            this._introduce = introduce;
+
+            if (previousDay != null)
+            {
+                this._dayProfit = (this._capital - previousDay._capital) - this._introduce;
+                this._cumProfit = previousDay._cumProfit + this._dayProfit;
+
+                Money dd = previousDay._drawDown - this._dayProfit;
+                this._drawDown = (dd < 0) ? 0 : dd;
+
+                this._cumProfitPC = previousDay._cumProfitPC + this.DayProfitPC;
+            }
+            else
+            {
+                this._dayProfit = 0;
+                this._cumProfit = this._dayProfit;
+                this._drawDown = (this._dayProfit > 0) ? 0 : this._dayProfit;
+                this._cumProfitPC = this.DayProfitPC;
+            }
+        }
+        #endregion
+
         #region Properties
         public DateTime Date
         {
