34b00e65b71302b3a086e3caeef91005da9575e1
elp87 <elp1987@gmail.com>
2014-10-28 13:20:47 +0300

    Graphs: Add PointData class

---
+++ elp87.Finance/elp87.Finance.Graphs/PointData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace elp87.Finance.Graphs
+{
+    public class PointData
+    {
+        public DateTime Date { get; set; }
+
+        public Money Value { get; set; }
+    }
+}
