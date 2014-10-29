cfa788d354e251d44eb334de7c5a84cd95c5469b
elp87 <elp1987@gmail.com>
2014-10-29 16:48:29 +0300

    Graphs: Add GraphProperty, GraphData classes

---
+++ elp87.Finance/elp87.Finance.Graphs/GraphData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace elp87.Finance.Graphs
+{
+    public class GraphData
+    {
+        public List<PointData> Points { get; set; }
+
+        public GraphProperty Property { get; set; }
+    }
+}
---
+++ elp87.Finance/elp87.Finance.Graphs/GraphProperty.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media;
+
+namespace elp87.Finance.Graphs
+{
+    public class GraphProperty
+    {
+        /// <summary>
+        /// Возвращает или задает объект <see cref="System.Windows.Media.Brush"/>, определяющий способ рисования линии графика
+        /// </summary>
+        public Brush Stroke { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает объект <see cref="System.Windows.Media.Brush"/>, определяющий способ рисования внутренней части графика
+        /// </summary>
+        public Brush Fill { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коэффициент непрозрачности графика
+        /// </summary>
+        public double Opacity { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает толщину линии графика
+        /// </summary>
+        public double StrokeThickness { get; set; }
+    }
+}
