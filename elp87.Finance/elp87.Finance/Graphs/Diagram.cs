13f1c9e5b4ae2048b1b5a9e60dbadfb31d8284d6
elp87 <elp1987@gmail.com>
2014-11-14 18:15:15 +0300

    Graphs: Add DiagramCategoryData

---
+++ elp87.Finance/elp87.Finance/Graphs/Diagram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace elp87.Finance.Graphs
+{
+    public abstract class Diagram
+    {
+    }
+}
---
+++ elp87.Finance/elp87.Finance/Graphs/DiagramCategoryData.cs
@@ -0,0 +1,9 @@
+﻿namespace elp87.Finance.Graphs
+{
+    public class DiagramCategoryData
+    {
+        public string Title { get; set; }
+
+        public double Value { get; set; }
+    }
+}
