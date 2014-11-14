a94ade4195918b7cf121bc0e918258a74bf7a133
elp87 <elp1987@gmail.com>
2014-11-14 18:46:05 +0300

    Graphs: Diagram (start working)

--- elp87.Finance/elp87.Finance/Graphs/Diagram.cs
+++ elp87.Finance/elp87.Finance/Graphs/Diagram.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace elp87.Finance.Graphs
 {
-    public abstract class Diagram
+    public abstract class Diagram : IGraph
     {
+        #region Fields
+        protected List<DiagramCategoryData> _categories;
+        protected DiagramType _diagramType;
+        #endregion
+
+        #region Methods
+        public void DrawGraph()
+        {
+            
+        }
+        #endregion
+
+        #region Enums
+        public enum DiagramType
+        {
+            HorizontalBlocs,
+            VerticalBlocs
+        } 
+        #endregion
     }
 }
