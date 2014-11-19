963f47ce36560719f72431081772af27eaea6abb
elp87 <elp1987@gmail.com>
2014-11-19 17:39:28 +0300

    Graphs: Add MonthEquityDiagram

---
+++ elp87.Finance/elp87.Finance/Graphs/MonthEquityDiagram.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace elp87.Finance.Graphs
+{
+    public abstract class MonthEquityDiagram : Diagram
+    {
+        protected MonthEquityDiagram(Grid grid)
+            : base(grid)
+        {
+            this._diagramType = DiagramTypes.VerticalBlocks;
+            this._fillingType = FillingTypes.MinToMaxGradient;
+        }
+    }
+}
