3888da1e014ac95b652ae841d42894934876947b
elp87 <elp1987@gmail.com>
2014-11-19 19:00:46 +0300

    Graphs: Add TradeDaysMonthEquityDiagram

---
+++ elp87.Finance/elp87.Finance/Graphs/TradeDaysMonthEquityDiagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace elp87.Finance.Graphs
+{
+    public class TradeDaysMonthEquityDiagram : MonthEquityDiagram
+    {
+        public TradeDaysMonthEquityDiagram(Grid grid, List<TradeDay> tradeDays)
+            : base(grid)
+        {
+            this._categories = new List<DiagramCategoryData>();
+            DateTime minDate = tradeDays.Min(day => day.Date);
+            DateTime minMonth = new DateTime(minDate.Year, minDate.Month, 1);
+            DateTime maxDate = tradeDays.Max(day => day.Date);
+            DateTime maxMonth = new DateTime(maxDate.Year, maxDate.Month, 1);
+
+            DateTime month = minMonth;
+            while (month <= maxMonth)
+            {
+                double monthProfit = tradeDays.Where(day => day.Date.Month == month.Month && day.Date.Year == month.Year).Sum(day => day.DayProfitPC);
+                this._categories.Add(new DiagramCategoryData() { Title = month.ToShortDateString(), Value = monthProfit });
+                month = month.AddMonths(1);
+            }
+        }
+    }
+}
