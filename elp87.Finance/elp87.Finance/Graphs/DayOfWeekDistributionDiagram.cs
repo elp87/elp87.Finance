e3f4ff8cd3b3a517ff9e9bc4a1fbcb80e155c49a
elp87 <elp1987@gmail.com>
2014-11-21 11:26:55 +0300

    Graphs: Add DayOfWeekDistributionDiagram

---
+++ elp87.Finance/elp87.Finance/Graphs/DayOfWeekDistributionDiagram.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace elp87.Finance.Graphs
+{
+    public abstract class DayOfWeekDistributionDiagram : Diagram
+    {
+        protected CalcTypes _calcType;
+
+        protected DayOfWeekDistributionDiagram(Grid grid)
+            : base(grid)
+        {
+            this._diagramType = DiagramTypes.HorizontalBlocks;
+            this._fillingType = FillingTypes.MinToMaxGradient;
+        }
+
+        protected enum CalcTypes
+        {
+            EntryDate,
+            ExitDate
+        }
+    }
+}
