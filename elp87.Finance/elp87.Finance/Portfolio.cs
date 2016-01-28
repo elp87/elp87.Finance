684f641dbbe3d0c28b88e6a8bd2c4094842c3880
elp87 <elp1987@gmail.com>
2016-01-28 17:25:43 +0300

    Add Portfolio class

---
+++ elp87.Finance/elp87.Finance/Portfolio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace elp87.Finance
+{
+    public class Portfolio
+    {
+        public List<TradeSystem> TradeSystems { get; set; }
+        public List<TradeDay> Days { get; set; }
+    }
+}
