2367b60e61ec60e87e92ef78ed387537d14f1a67
elp87 <elp1987@gmail.com>
2016-02-09 14:26:38 +0300

    Portfolio: Add ctor)))

--- elp87.Finance/elp87.Finance/Portfolio.cs
+++ elp87.Finance/elp87.Finance/Portfolio.cs
@@ -4,6 +4,11 @@ namespace elp87.Finance
 {
     public class Portfolio
     {
+        public Portfolio()
+        {
+            TradeSystems = new List<TradeSystem>();
+            Days = new List<TradeDay>();
+        }
         public List<TradeSystem> TradeSystems { get; set; }
         public List<TradeDay> Days { get; set; }
     }
